cce37cdf51c366a063aadfff9fc0fdcb21297a19
J0ke <cs.gamer0902@gmail.com>
2023-06-06 13:44:12 +0300

    push

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Значення і: " + i);
+        }
+        int x = 1;
+        while (x <= 5) {
+            System.out.println("Значення x: " + x);
+            x++;
+        }
+        int y = 1;
+        do {
+            System.out.println("Значення y: " + y);
+            y++;
+        } while (y <= 5);
+    }
+}
